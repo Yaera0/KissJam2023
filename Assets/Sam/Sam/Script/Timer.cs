a5a1f3ea70980aaa90bfe8c28b5355f5881b4f08
SamuelValken <s.valkenborgh@yahoo.com>
2023-05-06 18:37:55 +0200

    versiongood

--- Assets/Sam/Sam/Script/Timer.cs
+++ Assets/Sam/Sam/Script/Timer.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class Timer : MonoBehaviour
 {
-    public float TimeLeft;
-    public bool TimerOn = false;
-    public string TimerTxt;
-
-    private void Start()
-    {
-        
-    }
+    public float TimeLeft = 60;
+    public TMP_Text TimerText;
     void Update()
     {
-        
+        TimeLeft -= Time.deltaTime;
+        string TimeString = TimeLeft.ToString();
+        TimeString = string.Format("{0:00}", TimeLeft);
+        TimerText.SetText(""+ TimeString);
+        if (TimeLeft < 0.1)
+        {
+            if (GameObject.FindGameObjectWithTag("Finish")) 
+             {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+             }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
